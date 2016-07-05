756b8fe30ca7a6035a21138ae88e10c9db9cbb36
ro-double-b <robb.marsee@gmail.com>
2016-07-04 21:32:26 -0700

    Pass all test
    still need to make unique pages for each user and database

--- app/models/user.js
+++ app/models/user.js
@@ -5,6 +5,50 @@ var Promise = require('bluebird');
 
 
 var User = db.Model.extend({
+  tableName: 'users',
+  initialize: function() {
+    this.on('creating', this.hashPassword, this);
+  },
+  hashPassword: function(model, attrs, options) {
+    //should add to database
+    return new Promise(function(resolve, reject) {
+      //why do we need to wrap gensalt around hash?
+      //should passing in 10 as the salt work?
+      bcrypt.genSalt(10, function(err, salt) {
+        var salty = salt;
+        bcrypt.hash(model.attributes.password, salt, null, function(err, hash) {
+          if (err) { reject(err); }
+          model.set('password', hash);
+          model.set('salt', salty);
+          resolve(hash);
+        });
+      });
+    });
+  },
+ // check password which returns true or false
+  checkUser: function(salt, storedPassword, password, callback) {
+    var pw = storedPassword;
+    bcrypt.hash(password, salt, null, function(err, hash) {
+      console.log('inside checkUser!');
+      bcrypt.compare(password, pw, callback);
+    });
+    // console.log('inside checkUser');
+    // this.hashPassword()
+    // compare password, hash (db), and function(err, res)
+    // User.where('username', username).fetch().then(function(user) {
+    //   console.log('inside user.where', user.salt);
+    //   callback(err, user);
+    // });
+    //start by finding the username insde the database
+      //if it is found take the salt and the password
+        //apply the salt to the supplied password
+          //if it matches, let the user in
+          //else if password doesnt match or username doesnt match, notify user
+  // bcrypt.compare(password, hash, callback);
+    //for argument inputs, takes in a username and a cb
+    //go into the database
+    //and check if the username already exists
+  }
 });
 
 module.exports = User;
--- shortly.js
+++ shortly.js
@@ -13,6 +13,8 @@ var Click = require('./app/models/click');
 
 var app = express();
 
+// var user;
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
@@ -22,11 +24,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-
-app.get('/', 
-function(req, res) {
-  res.render('index');
+app.get('/login', function(req, res) {
+  res.render('login');
 });
+
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+// app.get('/', 
+// function(req, res) { 
+//   console.log('user inputed = ', req.body.username);
+//   console.log('user = ', user);
+
+//   if (!user === req.body.username) {
+//     res.redirect('/login');
+//   } else {
+//     res.render('index');
+//   }
+// });
 
 app.get('/create', 
 function(req, res) {
@@ -75,8 +91,157 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+var bcrypt = require('bcrypt-nodejs');
+var sessions = require('express-session');
+app.use(sessions({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+//this is the sign up
+app.post('/signup', function(request, response) {
+  // check database to see if user exists
+  var newUser = {
+    username: request.body.username,
+    password: request.body.password
+  };
+
+  new User(newUser).fetch().then(function(found) {
+    if (found) {
+      return 'user already in database';
+    } else {
+      // Users.checkUsers(newUser.username, cb) {
+      //   //console.log user name taken, choose a new one
+      //   //give an error status code
+
+      // }
+      Users.create({
+        username: newUser.username,
+        password: newUser.password
+      }).then(function(addedUser) {
+        // console.log('New user created....' + addedUser);
+        // response.status(200).send(addedUser);
+        response.redirect('/');
+      });
+    // if user does not exist first check if in database
+    // if not create a new instance on New User
+      
+    }
+  });
+});
+
+// var auth = function(req, res, next) {
+//   console.log(req.session, ' & ', req.session.user);
+//   if (req.session && req.session.user === 'Phillip') {
+//     return next();
+//   } else {
+//     return res.sendStatus(401);
+//   }
+// };
+
+app.post('/login', function(request, response) {
+  // alert('test test test')
+  var username = request.body.username;
+  var password = request.body.password;
+
+  // new User({username: username}).fetch().then(function(user) {
+  //   console.log(user);
+  // });
 
 
+  User.where('username', request.body.username).fetch().then(function(user) {
+    if (username = user.attributes.username) {
+
+      bcrypt.compare(password, user.attributes.password, function(err, data) {
+        if (err) { throw err; }
+        request.session.user = username;
+        console.log('Session user is...', request.session.user);
+        response.redirect('/');
+      });
+    } else {
+      response.render('/login');
+    }
+  }).catch(function(err) {
+    console.log('this error: ', err);
+    response.redirect('/login');
+  });
+});
+
+  //     User.checkUsers(user.attributes.salt, user.attributes.password, password, function(err, data) {
+  //       if (err) { throw err; }
+  //       request.session.user = username;
+  //       console.log('Session user is...', request.session.user);
+  //       response.redirect('/');
+  //     });
+  //   } else {
+  //     response.redirect('/login');
+  //   }
+  // }).catch(function(err) {
+  //   console.log(err);
+  // });
+
+
+  // if (request.body.username === 'Phillip' && request.body.password === 'Phillip') {
+  //   console.log('HIIIIIIIII');
+  //   request.session.user = 'Phillip';
+  //   console.log(request.session.user);
+  //   // user = request.session.user;
+  //   // response.send('login success!');
+  //   response.redirect('/');
+  // } else {
+  //   response.redirect('/login');
+  // }
+
+app.get('/', function(req, res) { 
+  if (req.session && req.session.user) {
+    if (!req.body.user === req.session.user) {
+      req.session.reset();
+      res.redirect('/login');
+    } else {
+      res.locals.user = req.body.user;
+      res.render('index');
+    }
+  } else {
+    res.redirect('/login');
+  }
+});
+  // console.log('user inputed = ', req.body.username);
+  // console.log('user = ', user);
+  // res.send();
+  // if (!user === req.body.username) {
+  //   res.redirect('/login');
+  // } else {
+  //   res.render('index');
+  // }
+//   new User({username: username}).fetch().then(function(user) {
+//     console.log(user.salt);
+//   });
+// });
+//     User.checkUsers(username, function(err, res) {
+//     if (err) { throw err; }
+//     console.log('final');
+//     console.log(res);
+//   });
+//   util.checkUsers(password, function(err, data) {
+//     if (err) { throw err; }
+//     if (!data) {
+
+//     }
+//
+
+//this is the login in page
+// app.post('/login', function(request, response) {
+//   var username = request.body.username;
+//   var password = request.body.password;
+
+//   if (username === 'Phillip' && password === 'Phillip') {
+//     response.redirect('/');
+//   } else {
+//     response.redirect('/login');
+//   }
+
+
+// });
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
