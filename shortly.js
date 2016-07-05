4593734f5e451f508a67fb4d679b2f15d5be8b74
ro-double-b <robb.marsee@gmail.com>
2016-07-05 13:05:22 -0700

    Added session cookies
    For log in and sign up

--- shortly.js
+++ shortly.js
@@ -101,14 +101,14 @@ app.use(sessions({
 //this is the sign up
 app.post('/signup', function(request, response) {
   // check database to see if user exists
-  var newUser = {
-    username: request.body.username,
-    password: request.body.password
-  };
+  var username = request.body.username;
+  var password = request.body.password;
 
-  new User(newUser).fetch().then(function(found) {
+
+  new User({username: username}).fetch().then(function(found) {
     if (found) {
-      return 'user already in database';
+      // response.send(500, 'showAlert');
+      response.redirect('/signup');
     } else {
       // Users.checkUsers(newUser.username, cb) {
       //   //console.log user name taken, choose a new one
@@ -116,38 +116,23 @@ app.post('/signup', function(request, response) {
 
       // }
       Users.create({
-        username: newUser.username,
-        password: newUser.password
+        username: username,
+        password: password
       }).then(function(addedUser) {
-        // console.log('New user created....' + addedUser);
-        // response.status(200).send(addedUser);
+        request.session.user = username;
+        app.locals.user = username;
+        console.log('Session user is...', request.session.user);
         response.redirect('/');
       });
-    // if user does not exist first check if in database
-    // if not create a new instance on New User
-      
     }
   });
 });
-
-// var auth = function(req, res, next) {
-//   console.log(req.session, ' & ', req.session.user);
-//   if (req.session && req.session.user === 'Phillip') {
-//     return next();
-//   } else {
-//     return res.sendStatus(401);
-//   }
-// };
 
 app.post('/login', function(request, response) {
   // alert('test test test')
   var username = request.body.username;
   var password = request.body.password;
-
-  // new User({username: username}).fetch().then(function(user) {
-  //   console.log(user);
-  // });
-
+  console.log('we are in login/POST: ', request.body)
 
   User.where('username', request.body.username).fetch().then(function(user) {
     if (username = user.attributes.username) {
@@ -155,6 +140,7 @@ app.post('/login', function(request, response) {
       bcrypt.compare(password, user.attributes.password, function(err, data) {
         if (err) { throw err; }
         request.session.user = username;
+        app.locals.user = username;
         console.log('Session user is...', request.session.user);
         response.redirect('/');
       });
@@ -167,81 +153,20 @@ app.post('/login', function(request, response) {
   });
 });
 
-  //     User.checkUsers(user.attributes.salt, user.attributes.password, password, function(err, data) {
-  //       if (err) { throw err; }
-  //       request.session.user = username;
-  //       console.log('Session user is...', request.session.user);
-  //       response.redirect('/');
-  //     });
-  //   } else {
-  //     response.redirect('/login');
-  //   }
-  // }).catch(function(err) {
-  //   console.log(err);
-  // });
-
-
-  // if (request.body.username === 'Phillip' && request.body.password === 'Phillip') {
-  //   console.log('HIIIIIIIII');
-  //   request.session.user = 'Phillip';
-  //   console.log(request.session.user);
-  //   // user = request.session.user;
-  //   // response.send('login success!');
-  //   response.redirect('/');
-  // } else {
-  //   response.redirect('/login');
-  // }
-
-app.get('/', function(req, res) { 
+app.get('/', function(req, res) {
+  console.log('this is the user: ', app.locals.user); 
   if (req.session && req.session.user) {
-    if (!req.body.user === req.session.user) {
+    if (app.locals.user === req.session.user) {
+      res.render('index');
+    } else {
       req.session.reset();
       res.redirect('/login');
-    } else {
-      res.locals.user = req.body.user;
-      res.render('index');
     }
   } else {
     res.redirect('/login');
   }
 });
-  // console.log('user inputed = ', req.body.username);
-  // console.log('user = ', user);
-  // res.send();
-  // if (!user === req.body.username) {
-  //   res.redirect('/login');
-  // } else {
-  //   res.render('index');
-  // }
-//   new User({username: username}).fetch().then(function(user) {
-//     console.log(user.salt);
-//   });
-// });
-//     User.checkUsers(username, function(err, res) {
-//     if (err) { throw err; }
-//     console.log('final');
-//     console.log(res);
-//   });
-//   util.checkUsers(password, function(err, data) {
-//     if (err) { throw err; }
-//     if (!data) {
 
-//     }
-//
-
-//this is the login in page
-// app.post('/login', function(request, response) {
-//   var username = request.body.username;
-//   var password = request.body.password;
-
-//   if (username === 'Phillip' && password === 'Phillip') {
-//     response.redirect('/');
-//   } else {
-//     response.redirect('/login');
-//   }
-
-
-// });
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
